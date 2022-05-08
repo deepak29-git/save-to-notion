4f45b650e635019608e6cd0cb739e2e33619990c
Arpit <arpitkumar01923@gmail.com>
2022-05-08 23:34:42 +0530

    add extension

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-
 import "./App.css";
 import { ToolTip } from "./Components";
 import { useState } from "react";
-import { Button } from './Components/Button/Button';
-import { Modal } from './Components/Modal/Modal';
-
+import { Modal2 } from "./Components/Modal2/Modal2";
 
 function App() {
+  const [title, setTitle] = useState("");
   const [data, setData] = useState("");
   const [isDone, setIsDone] = useState(false);
   return (
-
     <div className="App">
-      <ToolTip setData={setData} setIsDone={setIsDone} />
-      {isDone && <h1>{data}</h1>}
-    <div>
-      <Modal/>
-      <Button/>
-
+      <ToolTip setData={setData} setIsDone={setIsDone} setTitle={setTitle} />
+      <Modal2 title={title} setTitle={setTitle} />
     </div>
   );
 }
