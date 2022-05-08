80d5079511b90c896c5d565c9c76fb8d29a0da79
Arpit <arpitkumar01923@gmail.com>
2022-05-08 11:57:52 +0530

    use good naming convention and add colors as variable

--- src/Components/Tooltip/Tooltip.jsx
+++ src/Components/Tooltip/Tooltip.jsx
@@ -4,35 +4,37 @@ import "./Tooltip.css";
 export const ToolTip = ({setData,setIsDone}) => {
   const [range, setRange] = useState({ x: "0px", y: "0px" });
   const pos = { top: range.x, left: range.y };
-  const [show, setShow] = useState(false);
+  const [showTooltip, setShowTooltip] = useState(false);
 
   const buttonHandler = () => {
     const data = window.getSelection().toString();
     setData(data);
-    setIsDone(true)
+    setIsDone(true);
     window.getSelection().removeAllRanges();
-    setShow(false);
+    setShowTooltip(false);
   };
 
   const textHandler = (e) => {
     const data = window.getSelection().toString();
     if (data !== "") {
-      setShow(true);
-      setIsDone(false)
+      setShowTooltip(true);
+      setIsDone(false);
     }
     setRange({ y: `${e.pageX}px`, x: `${e.pageY}px` });
   };
 
   useEffect(() => {
     window.addEventListener("click", textHandler);
-  }, []);
+  });
 
   return (
     <>
-      {show && (
+      {showTooltip && (
+        <span>
           <button className="save" style={pos} onClick={() => buttonHandler()}>
             save
           </button>
+        </span>
       )}
     </>
   );
